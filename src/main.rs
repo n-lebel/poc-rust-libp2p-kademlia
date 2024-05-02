93bb81c19d158ff6af719b16c052b22e6bc107ea
n-lebel <nicklbch@gmail.com>
2024-05-02 11:00:15 +0200

    Simplify loop

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::time::Duration;
 use tokio;
 use log::{info, error, warn};
 use env_logger::{Env, Builder};
+use std::num::NonZeroUsize;
 
 use libp2p::{
     Multiaddr,
@@ -13,8 +14,6 @@ use libp2p::{
     PeerId,
     StreamProtocol, 
     SwarmBuilder,
-    tcp::Config as TcpConfig,
-    yamux::Config as YamuxConfig
 };
 
 use libp2p::futures::StreamExt;
@@ -61,11 +60,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let mut swarm = SwarmBuilder::with_existing_identity(local_key.clone())
         .with_tokio()
-        .with_tcp(
-            TcpConfig::default(), 
-            NoiceConfig::new, 
-            YamuxConfig::default 
-        )?
+        .with_quic()
         .with_behaviour(|key| {
 
             let local_peer_id = PeerId::from(key.clone().public());
@@ -92,20 +87,20 @@ async fn main() -> Result<(), Box<dyn Error>> {
             AgentBehavior::new(kad, identify, rr_behavior)
 
         })?
-        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30)))
+        .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(30)).with_notify_handler_buffer_size(NonZeroUsize::new(10000).unwrap()))
         .build();
 
     swarm.behaviour_mut().set_server_mode();
     
     if let Some(addr) = args().nth(1) {
-        swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+        swarm.listen_on("/ip4/0.0.0.0/udp/0/quic-v1".parse()?)?;
         
         let remote: Multiaddr = addr.parse()?;
         swarm.dial(remote)?;
         info!("Dialed to: {addr}");
     } else {
         info!("Act as bootstrap node");
-        swarm.listen_on("/ip4/0.0.0.0/tcp/8000".parse()?)?;
+        swarm.listen_on("/ip4/0.0.0.0/udp/8000/quic-v1".parse()?)?;
     }
 
     let mut peers: HashMap<PeerId, Vec<Multiaddr>> = HashMap::new();
@@ -127,47 +122,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 IdentifyEvent::Pushed { peer_id, info } => info!("IdentifyEvent:Pushed: {peer_id} | {info:?}"),
                 IdentifyEvent::Received { peer_id, info } => {
                     info!("IdentifyEvent:Received: {peer_id} | {info:?}");
-                    peers.insert(peer_id, info.clone().listen_addrs);    
-
-                    for addr in info.clone().listen_addrs {
-                        let agent_routing = swarm.behaviour_mut().register_addr_kad(&peer_id, addr.clone());
-                        match agent_routing {
-                            RoutingUpdate::Failed => error!("IdentifyReceived: Failed to register address to Kademlia"),
-                            RoutingUpdate::Pending => warn!("IdentifyReceived: Register address pending"),
-                            RoutingUpdate::Success => {
-                                info!("IdentifyReceived: {addr}: Success register address");
-                            } 
-                        }
-
-                        _ = swarm.behaviour_mut().register_addr_rr(&peer_id, addr.clone());
-                        
-                        let local_peer_id = local_key.public().to_peer_id();
-                        let message = GreeRequest{ message: format!("Send message from: {local_peer_id}: Hello gaess") };
-                        loop_send_message(&mut swarm, peer_id, GreeRequest{ message: "Hello gaess".to_string() }).await;
-                    }
-
-                    info!("Available peers: {peers:?}");                        
+                    // As soon as we connect to a peer, send a large number of messages
+                    loop_send_message(&mut swarm, peer_id, GreeRequest{ message: "Hello gaess".to_string() }).await;
                 },
                 _ => {}
             },
             SwarmEvent::Behaviour(AgentEvent::RequestResponse(event)) => match event {
                 RequestResponseEvent::Message { peer, message } => {
-                    match message {
-                        RequestResponseMessage::Request { request_id, request, channel} => {
-                            received_messages += 1;
-                            info!("Received messages: {received_messages}");
-                            let local_peer_id = local_key.public().to_peer_id();
-                            let response = GreetResponse{ message: format!("Response from: {local_peer_id}: hello too").to_string() };
-                            let result = swarm.behaviour_mut().send_response(channel, response);
-                            if result.is_err() {
-                                let err = result.unwrap_err();
-                                // error!("Error sending response: {err:?}")
-                            } else {
-                                // info!("Sending a message was success")
-                            }
-                        },
-                        _ => {}
-                    }
+                    received_messages += 1;
+                    info!("Received messages: {received_messages}");
                 },
                 RequestResponseEvent::InboundFailure { peer, request_id, error } => {
                     warn!("RequestResponseEvent::InboundFailure -> PeerID: {peer} | RequestID: {request_id} | Error: {error}")
